6696f1b7513ac541c595bd838cfab22959cc1aad
rnjai <rnjailamba12@gmail.com>
2015-01-23 22:50:39 +0530

    Done all basic installations

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'users/new'
+
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+  root to: "static_pages#home"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
